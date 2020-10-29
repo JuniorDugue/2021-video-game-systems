ec2127f2dc1b0870119eaf4210719d576d932377
Junior Dugue <jrdugue@gmail.com>
2020-10-29 19:41:30 -0400

    Update [gameSystems].js

--- pages/[gameSystems].js
+++ pages/[gameSystems].js
@@ -10,9 +10,6 @@ export default function GameSystem() {
     <div className={styles.container}>
       <motion.h1 layoutId="header">{gamesystems}</motion.h1>
       <motion.img layoutId={gamesystems} className={styles["big-image"]} src={gamesystems + ".jpg"} />
-      <Image>
-        
-      </Image>
     </div>
   );
 }
