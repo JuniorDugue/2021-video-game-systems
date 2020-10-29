c388ab686d12683f6b1f987b8bf9da3396636871
juniordugue <jrdugue@gmail.com>
2020-10-29 18:58:48 -0400

    Removed the .obsidian folder, currently working on implementing the nextjs image optimization, return or breadcrumb feature

--- pages/[gameSystems].js
+++ pages/[gameSystems].js
@@ -10,6 +10,9 @@ export default function GameSystem() {
     <div className={styles.container}>
       <motion.h1 layoutId="header">{gamesystems}</motion.h1>
       <motion.img layoutId={gamesystems} className={styles["big-image"]} src={gamesystems + ".jpg"} />
+      <Image>
+        
+      </Image>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
 
@@ -7,13 +8,20 @@ export default function Home() {
     <div className={styles.container}>
       <motion.h1 layoutId="header">Shop</motion.h1>
       <section className={styles["gameSystems-container"]}>
-        {["ps5", "xboxs", "switch", "pc"].map((gamesystems) => (
+        {/* {["ps5", "xboxs", "switch", "pc"].map((gamesystems) => (
           <Link href={gamesystems}>
             <a>
               <motion.img layoutId={gamesystems} className={styles.image} src={gamesystems + ".jpg"} animate={{ scale: 1 }} whileHover={{ scale: 1.1 }} />
             </a>
-          </Link>
-        ))}
+          </Link> 
+   
+        ))}*/}
+
+        {/* <Link href={gamesystems}> */}
+          <a>
+            <Image src="/ps5.jpg" alt="playstation 5" width={300} height={300}></Image>
+          </a>
+        {/* </Link> */}
       </section>
     </div>
   );
